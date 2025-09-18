075567b30dd7ffa292df1eddea82ffbd7d3a3a75
Emanuel Rabina <emanuelrabina@gmail.com>
2025-09-18 18:12:52 +1200

    Bring over the main extension method shared so far

---
+++ test/nz/net/ultraq/groovy/extensions/JomlExtensionsTests.groovy
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2025, Emanuel Rabina (http://www.ultraq.net.nz/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package nz.net.ultraq.groovy.extensions
+
+import org.joml.primitives.Rectanglef
+import spock.lang.Specification
+
+/**
+ * Tests for the extension methods.
+ *
+ * @author Emanuel Rabina
+ */
+class JomlExtensionsTests extends Specification {
+
+	def 'Set all values of a rectangle in one method call'() {
+		given:
+			var rect = new Rectanglef()
+		when:
+			rect.set(1, 2, 3, 4)
+		then:
+			rect.minX == 1
+			rect.minY == 2
+			rect.maxX == 3
+			rect.maxY == 4
+	}
+}
